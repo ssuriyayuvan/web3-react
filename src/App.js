f86757beed50ada4b5281bef6e77477010636ccc
SuriyaSampath <suriyasingh001@gmail.com>
2022-04-26 21:07:43 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Web3 from 'web3';
+import { useEffect } from 'react';
+const web3 = new Web3('http://localhost:8545');
+
+
 
 function App() {
+  
+
+// console.log('wallet is', thulasiWallet1['0'].address, thulasiWallet2['0'].address,);
+  useEffect(()=> {
+      // addWallet()
+      // addWallet();
+  }, []);
+  const addwallet = async () =>{
+    const thulasiWallet1 = web3.eth.accounts.wallet.create(1, 'suriya ss');
+const thulasiWallet2 = web3.eth.accounts.wallet.create(1, 'ss');
+    // console.log('add wallet ',await web3.eth.accounts.wallet.add(privateKey));
+    console.log('walelt added ',  thulasiWallet1.add(thulasiWallet2['0'].privateKey))
+    // console.log(await web3.eth.getAccounts())
+     console.log('save wallet ',web3.eth.accounts.wallet.save('suriya ss'))
+    // console.log('wallet load', await wallet.clear('wallet'))
+    console.log('total ', web3.eth.accounts.wallet.load('suriya ss', 'myWalletKey'))
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={addwallet}>Save</button>
     </div>
   );
 }
