e88c705562ef59c0dbb3eb6fb363036519fd615e
SuriyaSampath <suriyasingh001@gmail.com>
2022-04-27 12:16:43 +0530

    password and wallet added are seperated

--- src/App.js
+++ src/App.js
@@ -1,31 +1,56 @@
 
 import Web3 from 'web3';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 const web3 = new Web3('http://localhost:8545');
 
 
 
 function App() {
-  
 
-// console.log('wallet is', thulasiWallet1['0'].address, thulasiWallet2['0'].address,);
-  useEffect(()=> {
-      // addWallet()
-      // addWallet();
+  const [password, setPassword] = useState('');
+
+  // console.log('wallet is', thulasiWallet1['0'].address, thulasiWallet2['0'].address,);
+  useEffect(() => {
+    // addWallet()
+    // addWallet();
   }, []);
-  const addwallet = async () =>{
-    const thulasiWallet1 = web3.eth.accounts.wallet.create(1, 'suriya ss');
-const thulasiWallet2 = web3.eth.accounts.wallet.create(1, 'ss');
-    // console.log('add wallet ',await web3.eth.accounts.wallet.add(privateKey));
-    console.log('walelt added ',  thulasiWallet1.add(thulasiWallet2['0'].privateKey))
-    // console.log(await web3.eth.getAccounts())
-     console.log('save wallet ',web3.eth.accounts.wallet.save('suriya ss'))
-    // console.log('wallet load', await wallet.clear('wallet'))
-    console.log('total ', web3.eth.accounts.wallet.load('suriya ss', 'myWalletKey'))
-}
+  const createWallet = async () => {
+    if (password) {
+      const thulasiWallet1 = web3.eth.accounts.wallet.create(1, password);
+      // const thulasiWallet2 = web3.eth.accounts.wallet.create(1, 'ss');
+      // console.log('add wallet ',await web3.eth.accounts.wallet.add(privateKey));
+      // console.log('walelt added ',  thulasiWallet1.add(thulasiWallet2['0'].privateKey))
+      // console.log(await web3.eth.getAccounts())
+      console.log('save wallet ', web3.eth.accounts.wallet.save(password, 'add'))
+      // console.log('wallet load', await wallet.clear('wallet'))
+      console.log('load wallet ', web3.eth.accounts.wallet.load(password, 'add'))
+    } else {
+      alert('Please type password');
+    }
+  }
+  const addAccountToWallet = () => {
+    try {
+      let wallet = web3.eth.accounts.wallet.load(password, 'add');
+     let account = web3.eth.accounts.create();
+     wallet.add(account.privateKey);
+     web3.eth.accounts.wallet.save(password, 'add')
+     console.log(web3.eth.accounts.wallet.load(password, 'add'))
+    } catch (error) {
+      console.log('error in add account to wallet', error.message, error.stack)
+    }
+  }
+
+  const showWallet = () => {
+    console.log(web3.eth.accounts.wallet.load(password, 'add'));
+  }
+
   return (
     <div className="App">
-     <button onClick={addwallet}>Save</button>
+      <label>Password</label>
+      <input type='text' value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={createWallet}>Create Wallet</button>
+      <button onClick={addAccountToWallet}>Add Account to Wallet</button>
+      <button onClick={showWallet}>Show Wallet</button>
     </div>
   );
 }
